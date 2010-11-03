bb27e86e5e9a6f2a736d2c5f84902c488e78fc3d
RagnarBR <johnnyahss@hotmail.com>
2010-11-03 17:39:08 +0000

    Baetti vid StreamReaderDaemi2

---
+++ StreamReaderDaemi2/StreamReaderDaemi2/StreamReaderDaemi2.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace StreamReaderDaemi2
+{
+    class StreamReaderDaemi2
+    {
+        static void Main(string[] args)
+        {
+            //Add using System.IO;
+            List<string> lines = new List<string>(); //Used to store the lines
+
+            using (StreamReader reader = new StreamReader("stuff.txt")) //Stuff.txt contains 5 lines of text
+            {
+                string line = null; 
+
+                while ((line = reader.ReadLine()) != null) //Reads line after line until reader.ReadLine is blank
+                {
+                    lines.Add(line); //Adds every line to the list
+                    Console.WriteLine(line); //Writes every line to console
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
